826aa7c9963b1139f81c565a26cc6da63dda5f4a
rouna <godemode12@gmail.com>
2023-01-20 23:51:48 +0530

    updated site

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ const App = () => {
             <article className="pb-10 bg-lime-200 mb-10">
                 <h1 className='text-4xl text-center pt-4'>Welcome</h1>
                 <hr className=""/>
-                <p className='text-center text-2xl pb-4'>This is simple text animation using React and little bit of CSS</p>
+                <p className='text-center text-2xl pb-2'>This is simple text animation using React and little bit of CSS</p>
+                <p className='text-center text-2xl pb-4 font-serif decoration-dashed underline text-orange-500 hover:text-purple-900'><a href='https://github.com/RounabhSahu/TypeWrititing_animation_react'>Github Link</a></p>
                 <hr className="border-4 border-green-600"/>
                 <ul className='text-2xl pl-60 pt-4'>
                     <li className='text-xl ml-20'><b>style</b> --default value: none --uses tailwind --styling for the text</li>
